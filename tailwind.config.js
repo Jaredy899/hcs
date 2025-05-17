e1393cd9589024bdd3720e027696373b6738de57
Jared Cervantes <jared@jaredcervantes.com>
2025-05-16 21:40:14 -0400

    meh

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
 const {} = require("tailwindcss/defaultTheme");
 
 module.exports = {
-  mode: "jit",
   darkMode: "class",
-  purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
