cad976efeefc7c9e27f7fad9c1ba57646766e61e
Jared Cervantes <jared@jaredcervantes.com>
2025-06-09 16:26:24 -0400

    fix root

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: process.env.NODE_ENV === 'production' ? '/hcs/' : '/',
+  base: '/',
   plugins: [
     react(),
     // The code below enables dev tools like taking screenshots of your site
